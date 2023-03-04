aafb58c753864131a4482c68a1a6a096af65b729
JK Gunnink <jgunnink@gmail.com>
2023-03-04 19:45:13 +0800

    namings

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,20 @@ use hyper::{Body, Request, Response, Server};
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn rustless(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new("Hello, Rustless!".into()))
 }
 
 #[tokio::main]
 async fn main() {
-    // We'll bind to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // Bind to 127.0.0.1:8080
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
     // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
+    // creates one from our `rustless` function.
     let make_svc = make_service_fn(|_conn| async {
         // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
+        Ok::<_, Infallible>(service_fn(rustless))
     });
 
     let server = Server::bind(&addr).serve(make_svc);
